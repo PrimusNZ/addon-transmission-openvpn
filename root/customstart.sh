508ab975a3213a653bbc051f27bdb4aef4f82a67
PrimusNZ <primus@primusnz.live>
2023-06-09 00:36:10 +1200

    Fix Paths

--- root/customstart.sh
+++ root/customstart.sh
@@ -6,6 +6,18 @@ if [ ! -d /share/transmission-openvpn/config ]; then
   chown -R abc:abc /share/transmission-openvpn/config
 fi
 
+if [ ! -d /media/Downloads/completed ]; then
+  echo "Creating /media/Downloads/completed"
+  mkdir -p /media/Downloads/completed
+  chown -R abc:abc /media/Downloads/completed
+fi
+
+if [ ! -d /media/Downloads/incomplete ]; then
+  echo "Creating /media/Downloads/incomplete"
+  mkdir -p /media/Downloads/incomplete
+  chown -R abc:abc /media/Downloads/incomplete
+fi
+
 if [ -d /share/transmission-openvpn/config/openvpn ]; then
   echo "Copying OpenVPN configurations"  
   cp -R /share/transmission-openvpn/config/openvpn/* /etc/openvpn/
